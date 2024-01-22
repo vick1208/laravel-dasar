336d3bda5dae8444ab9a86b46d5cb2ca7d786644
Vicky AS <vickyhuang808@gmail.com>
2024-01-22 09:27:44 +0700

    format

--- config/contoh.php
+++ config/contoh.php
@@ -1,13 +1,9 @@
-<?php 
-
-    return [
-        "author" => [
-            "first" => env('NAME_FIRST','Eko'),
-            "last" => env('NAME_LAST','Khannedy')
-        ],
-        "email" => "echo.khannedy@gmail.com",
-        "web" => "https://www.programmerzamannow.com"
-    ];
-
-
-?>
+<?php
+return [
+    "author" => [
+        "first" => env('NAME_FIRST', 'Eko'),
+        "last" => env('NAME_LAST', 'Khannedy')
+    ],
+    "email" => "echo.khannedy@gmail.com",
+    "web" => "https://www.programmerzamannow.com"
+];
--- tests/Feature/ConfigurationTest.php
+++ tests/Feature/ConfigurationTest.php
@@ -8,16 +8,16 @@ use Tests\TestCase;
 
 class ConfigurationTest extends TestCase
 {
-   public function testConfig()
-   {
-    $firstName = config('contoh.author.first');
-    $lastName = config('contoh.author.last');
-    $email = config('contoh.email');
-    $web = config('contoh.web');
+    public function testConfig()
+    {
+        $firstName = config('contoh.author.first');
+        $lastName = config('contoh.author.last');
+        $email = config('contoh.email');
+        $web = config('contoh.web');
 
-    self::assertEquals('Eko',$firstName);
-    self::assertEquals('Khannedy', $lastName);
+        self::assertEquals('Eko', $firstName);
+        self::assertEquals('Khannedy', $lastName);
         self::assertEquals('echo.khannedy@gmail.com', $email);
         self::assertEquals('https://www.programmerzamannow.com', $web);
-   }
+    }
 }
